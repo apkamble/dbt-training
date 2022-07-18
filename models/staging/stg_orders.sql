7d22016a2e157d5104e18f87678dd4835b9db510
apkambledev@outlook.com <apkambledev@outlook.com>
2022-07-18 20:38:39 +0000

    Developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+
+SELECT 
+-- FROM raw orders
+o.orderid, 
+o.orderdate, 
+o.shipdate, 
+o.shipmode, 
+o.ordercostsellingprice - o.ordercustprice as orderprofit,
+
+-- from raw customer
+c.customername, 
+c.segment, 
+c.country,
+
+-- from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_order') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p 
+on o.productid = p.productid
